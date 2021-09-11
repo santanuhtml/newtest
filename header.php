1f84b34621982f7c50e8f29ad2863cc6156a2b9c
santanuhtml <santanuchowdhury04@gmail.com>
2021-09-12 00:14:40 +0530

    done

--- header.php
+++ header.php
@@ -34,29 +34,9 @@
 		    <!-- Collect the nav links, forms, and other content for toggling -->
 		    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
 		      <ul class="nav navbar-nav navbar-right">
-		        <li><a href="index.html">Home</a></li>
-		        <li><a href="contact.html">Contact</a></li>
+		        <li><a href="index.php">Home</a></li>
+		        <li><a href="contact.php">Contact</a></li>
 		      </ul>
 		    </div><!-- /.navbar-collapse -->
 		  </div><!-- /.container-fluid -->
 		</nav>
-    <div class="container">
-    	<div class="row">
-    		<div class="col-md-8 col-md-offset-2">
-    			<h1>Welcom</h1>
-			    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-			    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-			    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-			    consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-			    cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-			    proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-    		</div>
-    	</div>
-    </div>
-
-    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-    <!-- Include all compiled plugins (below), or include individual files as needed -->
-    <script src="js/bootstrap.min.js"></script>
-  </body>
-</html>
